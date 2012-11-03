83dc368ad4f226661ea585afd07be829924696c3
Rui Tang <tangrui@zyeeda.com>
2012-11-03 11:47:37 +0800

    Add mock settings entity.

--- src/main/webapp/WEB-INF/app/config.js
+++ src/main/webapp/WEB-INF/app/config.js
@@ -3,7 +3,8 @@ exports.coala = {
     appPath: 'src/main/webapp/WEB-INF/app/',
 
     entityPackages : [
-        'com.zyeeda.framework.commons.organization.entity'
+        'com.zyeeda.framework.commons.organization.entity',
+        'com.zyeeda.directory'
     ],
 
     orms: [
