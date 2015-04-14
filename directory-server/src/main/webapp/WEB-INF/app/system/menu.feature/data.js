b9e2757017535d570f4d9195025bb11af1d1e21a
luqiudong <luqiudong@zyeeda.com>
2015-04-14 12:03:39 +0800

    添加菜单

---
+++ directory-server/src/main/webapp/WEB-INF/app/system/menu.feature/data.js
@@ -0,0 +1,5 @@
+
+
+exports.results = [
+	{id: "100", name: "组织管理", iconClass: "icon-key", path: "#feature/admin/viewport"}
+]
--- directory-server/src/main/webapp/WEB-INF/app/system/menu.feature/scaffold.js
+++ directory-server/src/main/webapp/WEB-INF/app/system/menu.feature/scaffold.js
@@ -1,30 +1,19 @@
-exports.style = 'tree';
+var {mark}  = require('cdeio/mark');
+var {json} = require('cdeio/response');
+var data = require('system/menu.feature/data');
 
 exports.filters = {
     defaults: {
-        exclude: {
-            menuItemFilter: ['parent(1)']
-        }
+        '!menuItemFilter': ['children']
     }
 };
 
-exports.labels = {
-    name: '名称',
-    description: '描述',
-    featurePath: '标识',
-    iconClass: '图标',
-    option: '配置'
-};
+exports.doWithRouter = function(router) {
+    router.get('/all', mark('services', 'system/menu').on(function(service) {
+        return json({results: service.list()}, exports.filters.defaults);
+    }));
 
-exports.fieldGroups = {
-    DEFAULT: [
-        'name',
-        'description',
-        'featurePath',
-        'iconClass', {
-            name: 'option',
-            type: 'textarea',
-            rowspan: 5
-        }
-    ]
+    router.get('/import', mark('services', 'system/menu').on(function(service) {
+        return json({results: service.import(data.results)});
+    }));
 };
