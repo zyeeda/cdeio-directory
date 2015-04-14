54a66885f5423798ea1e582acc97af77ce120b2c
luqiudong <luqiudong@zyeeda.com>
2015-04-14 16:53:31 +0800

    添加菜单数据库信息

---
+++ directory-server/src/main/resources/migration/mysql/V1.0.1__CDE_MENUITEM.sql
@@ -0,0 +1,5 @@
+INSERT INTO `CDE_MENUITEM` (`F_ID`, `F_DESC`, `F_ICON`, `F_NAME`, `F_OPENED`, `F_OPTION`, `F_PATH`, `F_RANK`, `F_PARENT_ID`)
+VALUES
+	('001','系统管理','icon-key','系统管理',0,NULL,'#',20,NULL),
+	('001001','用户组织机构管理','undefined','用户组织机构管理',0,NULL,'#feature/admin/viewport',25,'001'),
+	('001002','个人信息管理','undefined','个人信息管理',0,NULL,'#feature/profile/viewport',30,'001')
